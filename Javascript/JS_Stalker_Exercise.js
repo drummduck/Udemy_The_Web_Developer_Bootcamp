2ea46ae59404107e4c60e27ce3f323eabd777f87
drummduck <nathan.donaldsondrum@gmail.com>
2020-03-10 18:04:19 -0600

    JS_Stalker_Exercise

---
+++ Javascript/JS_Stalker_Exercise.js
@@ -0,0 +1,9 @@
+var firstName = "";
+var lastsName = "";
+var age = -1;
+
+firstName = prompt("What is your first name?");
+lastName = prompt("What is your last name?");
+age = prompt("How old are you?");
+
+console.log("Your first name is " + firstName + " and your last name is " + lastName + ".\n" + "You are " + age + " years old.");
